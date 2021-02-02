bf95f203455a9a3d4e1ed872e868c117e063e52c
Nick Foden <nickfoden@gmail.com>
2021-02-02 17:35:31 -0500

    set a status code

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ router.get("/hello", async (ctx) => {
   ctx.body = "Hello Hello Hello";
 });
 
-router.get("/hello/:id", async (ctx) => {
-  ctx.body = `Hello ${ctx.params.id}`;
+router.get("/hello/:name", async (ctx) => {
+  // update status code - default is 200
+  ctx.status = 100;
+  ctx.body = `Hello ${ctx.params.name}`;
+});
+
+router.get("/", async (ctx) => {
+  ctx.body = "Worldly Hellos from Koa";
 });
 
 app.use(router.routes()).use(router.allowedMethods());
