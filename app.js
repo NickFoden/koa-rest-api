2b9e8ceb097917e951c9c3155eb4531a9d1906d5
Nick Foden <nickfoden@gmail.com>
2021-02-02 18:17:14 -0500

    parse body of post requests check if static is being served

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const Koa = require("koa");
 const Router = require("@koa/router");
 const serve = require("koa-static");
+const bodyParser = require("koa-bodyparser");
 
 const app = new Koa();
+app.use(bodyParser());
+
 const router = new Router();
 
-// app.use(serve(__dirname + "/public"));
+app.use(serve(__dirname + "/public"));
 
 router.get("/hello", async (ctx) => {
   ctx.body = "Hello Hello Hello";
@@ -15,6 +18,10 @@ router.get("/hello/:name", async (ctx) => {
   // To update status code - (default is 200)
   // ctx.status = 100;
   ctx.body = `Hello ${ctx.params.name}`;
+});
+router.post("/json", async (ctx) => {
+  const { name = "World" } = ctx.request.body;
+  ctx.body = `Hello ${name}`;
 });
 
 router.get("/", async (ctx) => {
